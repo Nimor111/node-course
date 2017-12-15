a6376d435d7484ffc6ba852cf02d03f241d9871c
Nimor111 <georgi.bojinov@hotmail.com>
2017-12-15 21:52:59 +0200

    Add dynamic templates

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', hbs);
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'George',
-    likes: ['Cats', 'Evi'],
+  res.render('index.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my first express web app!',
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
 });
 
 app.get('/bad', (req, res) => {
