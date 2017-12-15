cdf2290e38e06b37a26390d697e16f96528acf61
Nimor111 <georgi.bojinov@hotmail.com>
2017-12-15 22:21:07 +0200

    Create and use middlewares

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express');
 const hbs = require('hbs');
 
@@ -5,6 +7,22 @@ const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', hbs);
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${req.method} ${req.url} - ${now}`;
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
