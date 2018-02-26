c1e4c69eb3bb9aa8c3393a795a45008c0962d4ce
Nimor111 <georgi.bojinov@hotmail.com>
2018-02-27 00:24:28 +0200

    Start todo api, add post todo route

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text,
+  });
+
+  newTodo.save().then(
+    todo => {
+      res.send(todo);
+    },
+    err => {
+      res.status(400).send(err);
+    },
+  );
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
