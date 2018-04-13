6f9a99d8f8afa372a56678f028cb0cea84417ec3
Nimor111 <georgi.bojinov@hotmail.com>
2018-04-13 22:21:24 +0300

    Setup socket io and simple messages on connect and disconnect

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// registers an event listener
+// listen for a new connection
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
