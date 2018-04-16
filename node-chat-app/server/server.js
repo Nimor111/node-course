1e29031678e0203327eead8d233750c7da3b6a2c
Nimor111 <georgi.bojinov@hotmail.com>
2018-04-16 12:28:44 +0300

    Send messages only to people in same room

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -54,17 +54,28 @@ io.on('connection', socket => {
   });
 
   socket.on('createMessage', (newMessage, callback) => {
-    console.log('Sent a message', newMessage);
+    const user = users.getUser(socket.id);
 
-    io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+    if (user && isRealString(newMessage.text)) {
+      io
+        .to(user.room)
+        .emit('newMessage', generateMessage(user.name, newMessage.text));
+    }
+
     callback();
   });
 
   socket.on('createLocationMessage', coords => {
-    io.emit(
-      'newLocationMessage',
-      generateLocationMessage('Admin', coords.latitude, coords.longitude),
-    );
+    const user = users.getUser(socket.id);
+
+    if (user) {
+      io
+        .to(user.room)
+        .emit(
+          'newLocationMessage',
+          generateLocationMessage(user.name, coords.latitude, coords.longitude),
+        );
+    }
   });
 
   socket.on('disconnect', () => {
